5d4bf44df84bf0c49211b7b5ce295d2add45b79e
a579749 <579749@stud.hvl.no>
2021-11-19 19:23:51 +0100

    ree

---
+++ src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
@@ -0,0 +1,97 @@
+package no.hvl.dat100.jplab11.oppgave1;
+
+import no.hvl.dat100.jplab11.common.TODO;
+
+public abstract class Innlegg {
+	
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
+
+
+
+	public Innlegg() {
+		
+	}
+	
+	public Innlegg(int id, String bruker, String dato) {
+		
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=0;
+
+		
+	}
+
+	public Innlegg(int id, String bruker, String dato, int likes) {
+
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=likes;
+		
+		
+	}
+	
+	public String getBruker() {
+		return this.bruker;
+		
+	}
+
+	public void setBruker(String bruker) {
+		this.bruker=bruker;
+		
+	}
+
+	public String getDato() {
+		return this.dato;
+		
+		
+	}
+
+	public void setDato(String dato) {
+		this.dato=dato;
+		
+	}
+
+	public int getId() {
+		return this.id;
+		
+
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+
+	public int getLikes() {
+		return this.likes;
+		
+
+	}
+	
+	public void doLike () {
+		likes+=1;
+	}
+	
+	public boolean erLik(Innlegg innlegg) {
+		if(id==innlegg.getId()){
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return getId()+"\n"+getBruker()+"\n"+getDato()+"\n"+getLikes()+"\n";
+				
+	}
+	
+	// Metoden nedenfor er kun for valgfri oppgave 6
+	public String toHTML() {
+		
+		throw new UnsupportedOperationException(TODO.method());
+				
+	}
+}
